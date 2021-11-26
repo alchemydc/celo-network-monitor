5444a53547626a507b1d6cac80e0bf0b5b4705f0
dc <dc@zanshindojo.org>
2021-11-26 18:57:52 +0000

    wrapper script for docker build, re-enable snyk protect for dependency assessment

--- docker/docker_build_monitor.sh
+++ docker/docker_build_monitor.sh
@@ -4,4 +4,4 @@ set -x
 VERSION=$(cat ../VERSION)
 
 cd ..
-docker build -f docker/Dockerfile -t celo-network-monitor:$VERSION .
+docker build -f docker/Dockerfile -t celo-network-monitor:$VERSION . && docker image tag celo-network-monitor:$VERSION celo-network-monitor:latest
