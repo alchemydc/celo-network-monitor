09c8d44ef6da74f59170f59a1033ed57f4971c06
dc <dc@zanshindojo.org>
2021-11-27 16:49:30 +0000

    unified docker-compose.yml for mainnet and baklava monitoring.

--- docker/docker_build_monitor.sh
+++ docker/docker_build_monitor.sh
@@ -6,6 +6,7 @@ VERSION=$(cat ../VERSION)
 echo "Deleting old full node data"
 sudo rm -rfv $HOME/celo-network-monitor/docker/mainnet/full-node-data/celo
 sudo rm -rfv $HOME/celo-network-monitor/docker/mainnet/full-node-data/keystore
+sudo rm -rfv $HOME/celo-network-monitor/docker/baklava/full-node-data/baklava
 
 cd ..
 docker build -f docker/Dockerfile -t celo-network-monitor:$VERSION . && docker image tag celo-network-monitor:$VERSION celo-network-monitor:latest
