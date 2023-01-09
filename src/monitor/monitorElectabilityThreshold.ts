8b0573845cade7518f3fa711b9625b3880ca5f1c
dc <dc@zanshindojo.org>
2023-01-09 22:33:44 +0000

    disabled handling of proposals in the queued and approval stages for now, as they break the new telemetry we have for proposals in the referendum stage.

--- src/monitor/monitorElectabilityThreshold.ts
+++ src/monitor/monitorElectabilityThreshold.ts
@@ -1,9 +1,12 @@
+// see https://github.com/celo-org/celo-monorepo/blob/master/packages/protocol/contracts/governance/Election.sol
+
 import MonitorBase from "./monitorBase";
 
 /** Monitor the threshold of votes needed to get elected */
 export default class MonitorElectabilityThreshold extends MonitorBase {
 	protected async run() {
 		const election = await this.kit.contracts.getElection();
+		// Groups must receive at least this fraction of the total votes in order to be considered in elections
 		const electabilityThreshold = (
 			await election.electabilityThreshold()
 		).toNumber();
@@ -18,7 +21,7 @@ export default class MonitorElectabilityThreshold extends MonitorBase {
 			const totalGroupVotes = (await election.getTotalVotesForGroup(group))
 				.dividedBy(1e18)
 				.toNumber();
-				console.debug("totalGroupVotes: + " + totalGroupVotes)
+				console.debug("totalGroupVotes: " + totalGroupVotes)
 			if (totalGroupVotes > 0) {
 				this.metrics.log(
 					"GroupVotes",
